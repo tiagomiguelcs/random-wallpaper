dbc75b699f6c55643ef76ae8f437615d5808797b
Tiago Simões <tiagomiguelcs@gmail.com>
2023-10-06 15:13:33 +0100

    The wallpaper is only changed after a x period of time. No changes are made if the wallpaper is not older than x minutes

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ mkdir ~/Pictures/wallpapers/
 mkdir ~/Pictures/twall/
 cp -r wallpapers ~/Pictures/
 cp ~/.bashrc ~/.bashrc.backup
-echo "${PWD}/rwallpaper" >> ~/.bashrc
+echo "${PWD}/rwallpaper 15" >> ~/.bashrc
