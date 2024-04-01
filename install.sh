5496fbd068d2a91b976d6f7d176f8ac8141e05c1
tiagomiguelcs <tiagomiguelcs@gmail.com>
2024-04-01 12:27:12 +0100

    Add wallpapers packages support to the main install script

--- install.sh
+++ install.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+package="generic" # Define Wallpaper Package
+if [ $# -gt 0 ]; then
+ package=$1 # Or set a Wallpaper Package provided by the user
+fi
+clear
+echo "# Installing Random-Wallpaper Script, using wallpaper package '$package'..."
 mkdir ~/Pictures/wallpapers/
 mkdir ~/Pictures/twall/
-cp -r wallpapers ~/Pictures/
+cp -r wallpapers/$package ~/Pictures/
+mv ~/Pictures/$package ~/Pictures/wallpapers/
 cp ~/.bashrc ~/.bashrc.backup
 echo "${PWD}/rwallpaper 15" >> ~/.bashrc
+echo "# All Done, Have a nice day!
+echo ""
