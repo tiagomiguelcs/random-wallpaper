0df9b0ba9d9e3ba981eaa0c787eaad762642d1b6
tiagomiguelcs <tiagomiguelcs@gmail.com>
2024-04-06 17:05:40 +0100

    Fix install script, added gnome support.

--- install.sh
+++ install.sh
@@ -8,8 +8,15 @@ echo "# Installing Random-Wallpaper Script, using wallpaper package '$package'..
 mkdir ~/Pictures/wallpapers/
 mkdir ~/Pictures/twall/
 cp -r wallpapers/$package ~/Pictures/
-mv ~/Pictures/$package ~/Pictures/wallpapers/
+cp ~/Pictures/$package/* ~/Pictures/wallpapers/
+rm -r ~/Pictures/$package
+
 cp ~/.bashrc ~/.bashrc.backup
 echo "${PWD}/rwallpaper 15" >> ~/.bashrc
-echo "# All Done, Have a nice day!
-echo ""
+echo "# Do you use Gnome? (Y/N)"
+read answer
+if [[ "$answer" == "Y" || "$answer" == "y" ]]; then
+   echo "gsettings set org.gnome.desktop.background picture-uri-dark file:///home/$USER/Pictures/twall/today.jpg" >> ~/.bashrc
+   echo "gsettings set org.gnome.desktop.background picture-uri file:///home/$USER/Pictures/twall/today.jpg" >> ~/.bashrc
+fi
+echo "# All Done, Have a nice day!"
